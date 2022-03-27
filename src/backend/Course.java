55cbaa1d4f98a87bb4584df55221cf861de77b92
Elijah Lee <finalfinish02@gmail.com>
2022-03-27 15:57:33 -0500

    added  java course

---
+++ src/backend/Course.java
@@ -0,0 +1,19 @@
+//build classes, conditiors 
+//
+//API in java 
+
+public class Course {
+	
+	//	className 	string
+	//profname 	string
+	//days 		[5]int
+	//startTime   string
+	//endTime		string
+	
+	
+	//CS120 Wang MoTuThFr 9:55-10:50 
+	//CS120 Wang MoTuThFr 11:00-11:55
+	
+	
+	
+}
